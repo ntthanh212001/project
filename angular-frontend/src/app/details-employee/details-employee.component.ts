3a217568e92176f2c7125c95294b5a7df8c44ce4
ntthanh212001 <nguyentrungthanh212001@gmail.com>
2022-02-17 21:19:05 +0700

    Initial commit

---
+++ angular-frontend/src/app/details-employee/details-employee.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
+
+@Component({
+  selector: 'app-details-employee',
+  templateUrl: './details-employee.component.html',
+  styleUrls: ['./details-employee.component.css']
+})
+export class DetailsEmployeeComponent implements OnInit {
+
+  id: number;
+  employee: Employee;
+  constructor(private route: ActivatedRoute, private employeService: EmployeeService) { }
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+
+    this.employee = new Employee();
+    this.employeService.getEmployeeById(this.id).subscribe( data => {
+      this.employee = data;
+    });
+  }
+
+}
